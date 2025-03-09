a89a6c6e8e52e348ca09ec6361b886650eb09a02
Divyesh <bhalodidivyesh@gmail.com>
2025-03-09 13:02:43 +0530

    #004 Create teh MVC Structure In Database

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+mod db;
+mod models;
+mod controllers;
+
+use controllers::setting_controller::{fetch_settings, update_settings};
 
 fn main() {
-    billing_lib::run()
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![fetch_settings, update_settings])
+        .run(tauri::generate_context!())
+        .expect("Error while running Tauri application");
 }
